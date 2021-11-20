ea860b5d43245e12703ad1e63615f8c81e7163b6
Christian Yao Komenan <kycdev@gmail.com>
2021-11-20 22:06:15 +0000

    commit

--- src/main/java/ci/gouv/dgbf/system/cloture/server/impl/service/ActServiceImpl.java
+++ src/main/java/ci/gouv/dgbf/system/cloture/server/impl/service/ActServiceImpl.java
@@ -1,9 +1,11 @@
 package ci.gouv.dgbf.system.cloture.server.impl.service;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 
 import org.cyk.utility.service.server.AbstractSpecificServiceImpl;
 
@@ -17,13 +19,25 @@ import ci.gouv.dgbf.system.cloture.server.impl.persistence.ActImpl;
 public class ActServiceImpl extends AbstractSpecificServiceImpl<ActDto,ActDtoImpl,Act,ActImpl> implements ActService,Serializable {
 
 	@Inject ActDtoImplMapper mapper;
-	@Inject ActBusiness operationBusiness;
+	@Inject ActBusiness business;
 	
 	public ActServiceImpl() {
 		this.serviceEntityClass = ActDto.class;
 		this.serviceEntityImplClass = ActDtoImpl.class;
 		this.persistenceEntityClass = Act.class;
 		this.persistenceEntityImplClass = ActImpl.class;
+	}
+
+	@Override
+	public Response lock(List<String> identifiers, String trigger) {
+		business.lock(identifiers, trigger);
+		return Response.ok().build();
+	}
+
+	@Override
+	public Response unlock(List<String> identifiers, String trigger) {
+		business.unlock(identifiers, trigger);
+		return Response.ok().build();
 	}
 
 }
